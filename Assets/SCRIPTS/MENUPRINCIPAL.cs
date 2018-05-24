5089bbac32327af309392ebb315c2277926ae5de
serlecu <36859810+serlecu@users.noreply.github.com>
2018-05-24 16:44:07 +0200

    Upload 1

---
+++ Assets/SCRIPTS/MENUPRINCIPAL.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MENUPRINCIPAL : MonoBehaviour {
+
+	public void Play(){
+		SceneManager.LoadScene ("PLAYMODE");
+
+	}
+
+
+	public void Intructions(){
+		SceneManager.LoadScene ("INSTRUCTIONS");
+    
+
+
+
+}
+
+		public void Quit(){
+			Application.Quit();
+			UnityEditor.EditorApplication.isPlaying=false;
+
+	}
+     }
+
+
