a67a9536bb9caefb84b15b75faf4a08ff684d23b
“Poligit config  --global user.email “polimujica@gmail.com <alumno@masteravm01.abbaa.upv.es>
2018-05-24 18:29:17 +0200

    Arreglo botones escena intructions

---
+++ Assets/SCRIPTS/MENUINSTRUCTIONS.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class MENUINSTRUCTIONS : MonoBehaviour {
+
+	public void Play(){
+		SceneManager.LoadScene ("PLAYMODE");
+
+	}
+
+
+	public void Back(){
+		SceneManager.LoadScene ("PANTALLA2");
+
+
+
+
+	}
+
+
+}
